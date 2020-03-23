39a669fb038fd507a392f578f782950748e7af8e
nitish97083 <nitishkumarverma25@gmail.com>
2020-03-24 00:12:45 +0530

    nothing

--- lib/home/SelectAddress.dart
+++ lib/home/SelectAddress.dart
@@ -12,10 +12,18 @@ class SelectAddress extends StatefulWidget {
 
 class SelectAddressState extends State<SelectAddress> {
   final _formKey = GlobalKey<FormState>();
+final  FocusNode _nameFocus = FocusNode();
+ final FocusNode _add1Focus = FocusNode();
+ final FocusNode _add2Focus = FocusNode();
+final  FocusNode _pinFocus = FocusNode();
+ final FocusNode _distFocus = FocusNode();
+final  FocusNode _contFocus = FocusNode();
+final  FocusNode _altconFocus = FocusNode();
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         backgroundColor: CustomColors.primaryColors,
       ),
@@ -27,22 +35,47 @@ class SelectAddressState extends State<SelectAddress> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Center(child: myTextWidget(text: 'Full Address')),
+                //Center(child: myTextWidget(text: 'Full Address')),
                 myTextWidget(text: 'Full Name'),
-                textFormField(context),
+                textFormField(context, "Please Enter Full Name",k: TextInputType.text,current: _nameFocus,next: _add1Focus),
                 myTextWidget(text: 'Address Line 1'),
-                textFormField(context),
+                textFormField(context, 'Please Enter Address',k:TextInputType.text,current: _add1Focus,next: _add2Focus ),
                 myTextWidget(text: 'Address Line 2'),
-                textFormField(context),
+                textFormField(context, 'Please Enter Address',k: TextInputType.text,current: _add2Focus,next: _pinFocus),
                 myTextWidget(text: 'Pin Code'),
-                textFormField(context),
-                myTextWidget(text: 'Distict'),
-                textFormField(context),
-                myTextWidget(text: 'Phone Number'),
-                textFormField(context),
+                textFormField(context, 'Please Enter PinCode',k: TextInputType.number,current: _pinFocus,next: _distFocus,pinLent: 6),
+                // myTextWidget(text: 'Distict'),
+                // textFormField(context, 'Please Enter Distict name',k: TextInputType.text,current: _distFocus,next: _contFocus),
+                myTextWidget(text: 'Phone Number', ),
+                textFormField(context, 'Please Enter Contact Number',k:TextInputType.number,current: _contFocus,next: _altconFocus),
                 myTextWidget(text: 'Alternate Phone Number'),
-                textFormField(context),
-                submitAddress(context, _formKey),
+                textFormField(context, null,k: TextInputType.number),
+                InkWell(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height / 20,
+                    margin: EdgeInsets.only(left: 10, right: 10, top: 10),
+                    color: Colors.grey.withOpacity(0.3),
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    child: Text('Save'),
+                  ),
+                  onTap: () {
+                    // Validate will return true if the form is valid, or false if
+                    //   // the form is invalid.
+                    if (_formKey.currentState.validate()) {
+                      // If the form is valid, we want to show a Snackbar
+                      //final snackbar = Scaffold.of(context)
+                      //     .showSnackBar(SnackBar(
+                      //       content: Text('Processing Data'),
+                      //     ))
+                      //     .closed
+                      //     .then((SnackBarClosedReason reason) {
+                      //   //  _opennewpage();
+                      // });
+                      _displaySnackBar(context);
+                    }
+                  },
+                ),
                 // else if(!isNumeric(value))
                 // {
                 //   return 'Please enter a valid Phone number';
@@ -61,6 +94,19 @@ class SelectAddressState extends State<SelectAddress> {
         ),
       ),
     );
+  }
+
+  _displaySnackBar(BuildContext context) {
+    final snackBar = SnackBar(
+      content: Text('Processing Data'),
+      backgroundColor: CustomColors.primaryColors,
+    );
+    _scaffoldKey.currentState
+        .showSnackBar(snackBar)
+        .closed
+        .then((SnackBarClosedReason reason) {
+      //  _opennewpage();
+    });
   }
 }
 
@@ -138,46 +184,33 @@ Widget myTextWidget({String text}) {
   );
 }
 
-Widget textFormField(context) {
+Widget textFormField(context, String validators,{TextInputType k,FocusNode current,FocusNode next,var pinLent}) {
   return Container(
-    height: MediaQuery.of(context).size.height / 22,
-    padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+    height: MediaQuery.of(context).size.height / 15,
+    padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
     child: TextFormField(
-      validator: (value) {
-        if (value.isEmpty) {
-          return "Please Enter Full Name";
-        }
-      },
+      keyboardType: k,
+      focusNode: current,
+      maxLength: pinLent,
+      validator: validators == null
+          ? null
+          : (value) {
+              if (value.isEmpty && value.length<=6) {
+                return validators;
+              }
+            },
+           onFieldSubmitted: (val){
+             _fieldFocusChange(context,current,next);
+           },
     ),
   );
 }
-
-Widget submitAddress(context, GlobalKey<FormState> _formkey) {
-  return  Container(
-      height:MediaQuery.of(context).size.height/20,
-      margin: EdgeInsets.only(left: 10,right: 10,top: 10),
-      color: Colors.grey.withOpacity(0.3),
-      alignment: Alignment.center,
-      width: MediaQuery.of(context).size.width,
-      child: InkWell(child: Text('Save'),
-       onTap: () {
-      // Validate will return true if the form is valid, or false if
-      //   // the form is invalid.
-      if (_formkey.currentState.validate()) {
-        // If the form is valid, we want to show a Snackbar
-        Scaffold.of(context)
-            .showSnackBar(SnackBar(
-              content: Text('Processing Data'),
-            ))
-            .closed
-            .then((SnackBarClosedReason reason) {
-          //  _opennewpage();
-        });
-      }
-    },
-    focusColor: Colors.red,
-      ),
-   
-   
-  );
+_fieldFocusChange(BuildContext context, FocusNode currentFocus,FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);  
 }
+
+//***************************************8 */
+// Widget submitAddress(context, GlobalKey<FormState> _formkey,GlobalKey<ScaffoldState> _scaffold) {
+//   return
+// }
